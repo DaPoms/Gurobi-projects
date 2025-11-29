3c7cb8b60f965846fbce32236af14930d5ee916b
DaPoms <DanielTripolitan@gmail.com>
2025-11-28 19:37:14 -0500

    MDMKP case 1 dataset 7 results

---
+++ MDMKP/fileread.cpp
@@ -0,0 +1,192 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+/* 
+NOTE: While this approach is less efficient than the fileread.cpp version, I decided on 
+this approach as it is more readable, and the heuristics will take long anyways, so a few extra 
+seconds of processing the data read from the file means nothing in the end
+ */
+//reads one block of data from MDMKP .txt file (one block = either <=, >= constraints or cost coefficient vals)
+/* 
+Function name: readAttributeOfMDMKP
+Params:
+    ifstream& file: The file we want to read from, should be "pointing" to the start of the attribute we want to read 
+    vector<vector<long>>& candidateCapacityAtrributes: Where we will store each candidates attribute for each constraint (reminder that there are multiple capacity constraints and demand constraints in each problem)
+    bool IsConstraint: if true, will read an additional line that will be the capacity/demand max/min(s) depending on the attribute we are reading. (<= constraint is capacity / max, >= constraint is demand/min)
+
+
+*/
+
+class MDMKRawProblem // each MDMKRawProblem is actually a set of 6 problems in 1 entity, but processing must be done for that to formm thus the "raw" name
+{
+    private:
+        vector<vector<long>> candidateCapacityAtrributes;
+        vector<vector<long>> candidateDemandAtrributes;
+        vector<vector<long>> candidateCostAtrributes;
+        vector<long> knapsackCapacityVals;
+        vector<long> knapsackDemandRequirementVals;
+    public:
+        vector<vector<long>>& getcandidateCapacityAtrributes() { return candidateCapacityAtrributes; } 
+        vector<vector<long>>& getcandidateDemandAtrributes() { return candidateDemandAtrributes; }
+        vector<vector<long>>& getCandidateValue() { return candidateCostAtrributes; }
+        vector<long>& getknapsackCapacityVals() { return knapsackCapacityVals; }
+        vector<long>& getknapsackDemandRequirementVals() { return knapsackDemandRequirementVals; }
+
+};
+
+struct MDMKCandidate
+{
+    vector<long> capacityVal; // How much capacity this candidate takes up for each capacity constraint
+    vector<long> demandVal; //How much demand value this candidate contributes
+    long value; // "Cost": how much this item is worth (either +/- depending on the case)
+};
+
+struct problemSet
+{
+    vector < vector <MDMKCandidate> > problemsByCase;
+    vector<long> knapsackCapacityVals;
+    vector<long> knapsackDemandRequirementVals; 
+};
+
+
+//Formats MKMDProblem into their 6 respective cases, in accordance to the brunel paper. Here is a definition of each case:
+/* 
+    Note that "q" is the number of demand constraints (>=) and "m" is the number of capacity (<=) constraints
+    These constraints are picked in order as they show up. EX: when q=1, it means only the first demand constraint is considered
+    Positive cost/value cases
+    Case 1: q = 1
+    Case 2: q = m/2
+    Case 3: q = m
+    Mixed cost/value cases
+    Case 4: q = 1
+    Case 5: q = m/2
+    Case 6: q = m
+*/
+void separateCandidatesByCases(MDMKRawProblem& problem, problemSet& candidatesByCase) 
+{
+    candidatesByCase.problemsByCase.resize(6); // new thing learned! Can be used to easily create uninitialized values inside of the vector (or reshape vector to fit this function's usage)
+    vector<vector<long>> MDMKCapacityAttributes = problem.getcandidateCapacityAtrributes(); 
+    vector<vector<long>> MDMKDemandAttributes = problem.getcandidateDemandAtrributes();
+    vector<vector<long>> MDMKValue = problem.getCandidateValue();
+    long capacityVarsCount = MDMKCapacityAttributes.size(); //Shortcut to finding the amount of capacity variables (dimensions) for the problem
+    long candidateCount = MDMKCapacityAttributes[0].size();//This is a way to find the # of candidates we have without having to pass directly (not too efficient, but less params = simpler). Only works if problem is not empty
+    int caseDemand1 = 1; //sort of pointless but slightly more readable as it describes what the value 1 is for (Case 1 of MDMKP problems)
+    int caseDemand2 = capacityVarsCount / 2;
+    int caseDemand3 = capacityVarsCount; // Note that case 1,2,3 are repeated for case 4,5,6 respectively
+     int order[6] = {0,3,1,4,2,5}; //order which we want to get candidates (most efficient order for the cases)
+    for(int i{0}; i < candidateCount; i++)
+    {
+        MDMKCandidate candidate;
+        for(int c{0}; c < capacityVarsCount; c++)
+            candidate.capacityVal.push_back(MDMKCapacityAttributes[c][i]);
+        
+       
+        for(int caseNum{0}; caseNum < 6; caseNum++) //Changes candidate to each case (note that caseNum = 0 is case 1 and caseNum = 5 is case 6)
+        {
+            int currentCase = order[caseNum];
+            candidate.value = MDMKValue[currentCase][i];
+
+            for(int currDemand{0}; currDemand < capacityVarsCount; currDemand++) //stops at case 6 (where same # of demands as capacity constraints)
+            {
+                // WARNING: Very ugly if statement below!
+                candidate.demandVal.push_back(MDMKDemandAttributes[currDemand][i]);
+
+                if ( (currDemand == caseDemand3 - 1 && (currentCase == 2 || currentCase == 5)) || (currDemand == caseDemand2 - 1 && (currentCase == 1 || currentCase == 4) ) || (currDemand == caseDemand1 - 1 && (currentCase == 0 || currentCase == 3)) ) //these checks are really inefficient but they work!
+                {
+                    candidatesByCase.problemsByCase[currentCase].push_back(candidate);
+                    
+                }      
+            }
+            
+            candidate.demandVal.clear();///
+        }
+        
+    }
+}
+
+void RawProblemsToCases(vector<MDMKRawProblem>& problems, vector<problemSet>& cases)
+{
+    problemSet candidatesByCase;
+    candidatesByCase.problemsByCase.resize(6);
+    //cases.resize(0);
+    /* for(problemSet problem : cases)
+        problem.problemsByCase.resize(6); */
+    
+    int size = problems.size();
+    //for(MDMKRawProblem problem : problems)
+    for(int i{0}; i < size; i++)
+    {
+        
+        separateCandidatesByCases(problems[i], candidatesByCase);
+        cases.push_back(candidatesByCase);
+        cases[i].knapsackCapacityVals = problems[i].getknapsackCapacityVals();
+        cases[i].knapsackDemandRequirementVals = problems[i].getknapsackDemandRequirementVals();
+        candidatesByCase.problemsByCase.clear();
+    }
+}
+
+
+void readAttributeOfMDMKP(ifstream& file, vector<vector<long>>& candidateCoefficientAtrributes, vector<long>& knapsackGoalVals, long candidateCount, long leConstraints, bool isConstraint)
+{
+    long placeholder; 
+    //leConstraints means <= constraints (also known as capacity constraints)
+    for(int m{0}; m < leConstraints; m++) //reading for all candidate constraints
+    {
+        candidateCoefficientAtrributes.push_back(vector<long>());
+        for(int i{0}; i < candidateCount; i++) // reading all <
+        {
+            file >> placeholder;
+            candidateCoefficientAtrributes[m].push_back(placeholder);
+        }
+    }
+    if(isConstraint)
+        for(int m{0}; m < leConstraints; m++) //Reads capacity of knapsack for each capacity constraint
+        {
+            file >> placeholder;
+            knapsackGoalVals.push_back(placeholder);
+        }
+}
+
+
+//Param of fileName is the file we want to read from, must be in the same folder as this file (though I could easily change this if needed)
+void readMDMKP(string fileName, vector<MDMKRawProblem>& MDMKRawProblems) // read MDMKP problem text files in accordance to the format done by https://people.brunel.ac.uk/~mastjjb/jeb/orlib/mdmkpinfo.html
+{
+    
+    ifstream file{fileName};
+    long testProblemCount, candidateCount, leConstraints; //leConstraints means <= constraints (also known as capacity constraints)
+    
+    file >> testProblemCount;
+
+    for(int i{0}; i < testProblemCount; i++) //traverses all the problem sets of the file 
+    {
+        MDMKRawProblem problemSet; //creates a new empty problem set for every instance
+        file >> candidateCount >> leConstraints; // These are the "header" variables for the brunel samples, they apply to all cases of a single problem (6 cases)
+        vector<vector<long>> candidateCapacityAtrributes; //each vector contains a separate attribute for every candidate 
+        vector<long> knapsackCapacityVals;
+        readAttributeOfMDMKP(file, problemSet.getcandidateCapacityAtrributes(), problemSet.getknapsackCapacityVals(), candidateCount, leConstraints, true); // reads <= constraint
+
+        vector<vector<long>> candidateDemandAtrributes;
+        vector<long> knapsackDemandRequirementVals;
+        readAttributeOfMDMKP(file, problemSet.getcandidateDemandAtrributes(), problemSet.getknapsackDemandRequirementVals(), candidateCount, leConstraints, true); // reads >= constraint
+
+        vector<vector<long>> candidateCostAtrributes; //Note that "knapsackCapacityVals" is useless here due to the boolean parameter being = false
+        //I repurposed the leconstraints parameter to be the # of cases each model has, which is 6 for Brunel test cases
+        readAttributeOfMDMKP(file, problemSet.getCandidateValue(), problemSet.getknapsackCapacityVals(), candidateCount, 6, false); // reads value of each object (Though the Brunel paper calls these "Cost" coefficients). 
+        
+        MDMKRawProblems.push_back(problemSet);
+    }
+}
+
+/* int main()
+{
+    vector<MDMKRawProblem> MDMKRawProblems;
+    readMDMKP("datac7.txt", MDMKRawProblems);
+    vector<vector<MDMKCandidate>> candidatesByCase;
+    vector<problemSet> problemSets;
+    RawProblemsToCases(MDMKRawProblems, problemSets);
+    return 0;
+} */
+
