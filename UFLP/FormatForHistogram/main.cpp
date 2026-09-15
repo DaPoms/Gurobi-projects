d5f3ab8f8871d591f6106b5ece96981a9e55c759
DaPoms <DanielTripolitan@gmail.com>
2026-09-15 09:00:48 -0400

    Histogram code for measuring UFLP cost distribution

--- UFLP/FormatForHistogram/main.cpp
+++ UFLP/FormatForHistogram/main.cpp
@@ -188,10 +188,10 @@ void runGurobiUFLP(GRBEnv& env, ofstream& excel, UFLPInstance& UFLProblem)
 
 vector<int> getOccurencesOfServiceCostRanges(UFLPInstance& problem) //gets the occurences of cost of service cost
 {
-    vector<int> ans(25); //chose vector for ease of use even though size is fixed
+    vector<int> ans(26); //chose vector for ease of use even though size is fixed
     for(double d : problem.servicePrices)
     {
-        int i = (int) d - 1;
+        int i = (int) d;
         ans[i]++;
     }
     return ans;
@@ -203,13 +203,13 @@ int main()
     excel << "Histograms:" << "\n";
 
     //reading + solving
-    fs::path problemFolderPath = "C:/Users/Pomer/Desktop/Gurobi projects/UFLP/standard_UFLP/problem_sets_(from_other_people)/Cadiz_1000-2000_MT1";
+    fs::path problemFolderPath = "C:/Users/Daniel Worktop/VsProjects/Gurobi_research/Gurobi-projects/UFLP/standard_UFLP/problem_sets_(from_other_people)/Cadiz_1000-2000_MT1";
     for(const fs::directory_entry& problemPath : fs::recursive_directory_iterator(problemFolderPath))
     {
         UFLPInstance UFLP;
-        readUFLP(problemPath.path().filename().string(), UFLP);
+        readUFLP(problemPath.path().string(), UFLP);
         vector<int> histogramData = getOccurencesOfServiceCostRanges(UFLP);
-        excel << problemPath.path().string() << "\n";
+        excel << problemPath.path().filename().string() << "\n";
         for(int i{0}; i < histogramData.size(); i++)
             excel << histogramData[i] << ",";
         excel << endl;
